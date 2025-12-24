7b2a79f1b56d89f3bd4de60f273713a48d4cd2ee
akiWagashi <qq1076363319@gmail.com>
2025-12-24 20:42:36 +0800

    fix wezterm.lua template

--- wezterm.lua
+++ wezterm.lua
@@ -2,20 +2,14 @@ local Config = require('config')
 
 require('utils.backdrops')
    -- :set_focus('#000000')
-   :set_images_dir('D:\\style\\background')
-   :set_images()
-   :random()
+   -- :set_images_dir(require('wezterm').home_dir .. '/Pictures/Wallpapers/')
+   -- :set_images()
+   -- :random()
 
 require('events.left-status').setup()
 require('events.right-status').setup({ date_format = '%a %H:%M:%S' })
 require('events.tab-title').setup({ hide_active_tab_unseen = false, unseen_icon = 'circle' })
 require('events.new-tab-button').setup()
-
-local localpwsh = { 'D:\\devEnvironment\\terminalTools\\powershell\\7\\pwsh.exe', '-NoLogo' }
-
-local launch = require('config.launch')
-               :insert_menu({label = 'pwsh' , args = localpwsh})
-               :set_default(localpwsh)
 
 return Config:init()
    :append(require('config.appearance'))
@@ -23,4 +17,4 @@ return Config:init()
    :append(require('config.domains'))
    :append(require('config.fonts'))
    :append(require('config.general'))
-   :append(launch.options).options
+   :append(require('config.launch')).options
